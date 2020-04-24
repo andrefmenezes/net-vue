09b2d61339b5346a1efd1228a9ca935dede9c54f
andrefmenezes <decomenezes00@gmail.com>
2020-04-24 12:23:05 -0300

    ok

---
+++ netflix/src/components/Filme.vue
@@ -0,0 +1,29 @@
+<template>
+  <div ref="scroller" class="row">
+        <div class="row__inner">
+          <div class="gui-card">
+            <div class="gui-card__media">
+              <img class="gui-card__img" :src="imagem" alt=""  />
+            </div>
+            <div class="gui-card__details">
+              <div class="gui-card__title">
+                {{titulo}}
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
+
+</template>
+
+<script>
+export default{  
+  props:['titulo','imagem']
+}
+
+</script>
+
+<style lang="scss">
+
+
+</style>
